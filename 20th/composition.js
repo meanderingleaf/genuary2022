f049ff80ee10c61cb257bce58ff6b298880e8222
Travis Faas <travis.faas@gmail.com>
2022-01-21 09:43:58 -0500

    Shape sea

--- 20th/composition.js
+++ 20th/composition.js
@@ -6,7 +6,7 @@ function setup() {
   angleMode(DEGREES);
 
   c1 = color(27, 24, 200);
-  c2 = color(255, 47, 181);
+  c2 = color(3, 177, 252);
   stroke(255);
 
   background(45);
@@ -22,30 +22,32 @@ function draw() {
   let nn = (noise(frameCount / 20) - 1) * 20;
 
   for(var i = 0; i < 30; i++) {
+    let e = Math.log(i)/Math.log(1.2)
+
     for(var x = 0; x < 30; x++) {
       let n = noise((x+oX)/20, (oY+i)/20, x/20);
       nn = n * 50;
       if(n>.65) {
 
         let c = lerpColor(c2,c1, n);
-        c = color(255);
+        c = color(255, 246, 209);
         c.setAlpha( 10 );
         fill(c);
         strokeWeight(2);
         
-        circle(x*12, i*12, n*30 );
+        circle(x*20 + nn, e*25 + nn, e*4 );
       } else if(n > .3) {
         let c = lerpColor(c1,c2, n);
         fill(c);
-        stroke(255);
-        noStroke();
-        strokeWeight(n*2);
-        circle(x*20 + nn, i*20 + nn, n*30 );
+        stroke(255, 246, 209);
+        //noStroke();
+        //strokeWeight(n*2);
+        circle(x*20 + nn, e*25 + nn, e*4 );
       } else {
         let c = lerpColor(c1,c2, n);
         fill(c);
-        noStroke();
-        circle(x*20 + nn, i*20 + nn, n*50 );
+        //noStroke();
+        circle(x*20 + nn, e*25 + nn, e*4 );
       }
     }
   }
