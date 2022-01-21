bc613439d374e62884271ca6dfa6a5b2803b729c
Travis Faas <travis.faas@gmail.com>
2022-01-21 08:15:14 -0500

    Final fixes on base composition

--- 19th/composition.js
+++ 19th/composition.js
@@ -9,38 +9,38 @@ function setup() {
   pwidth = textWidth(phrase);
   angleMode(DEGREES);
 
-  c1 = color(255,0,0);
-  c2 = color(255,255,0);
+  c1 = color(235, 255, 255);
+  c2 = color(21, 200, 0);
 
   for(let y = 0; y < 20; y++) {
     reps[y] = new Array();
     for(let i = 0; i < 15; i++) {
-      reps[y][i] = i * pwidth;
+      reps[y][i] = i * pwidth  - y*30;
     }
   }
 }
 
 function draw() {
-  background(255,255,255,30);
+  background(255,255,255, 10);
  
   //rotate(45);
   for(let z = 0; z < 3; z++) {
     rotate(-45);
-    translate(-400,200);
+    translate(-350,150);
     for(let y = 0; y < reps.length; y++) {
 
       //rotate(180);
       fill(0,0,0,20);
-      rect(0,y*30,1000,30);
+      noFill()
+      strokeWeight(1)
+      //rect(0,y*30,1000,30);
 
       for(let i = 0; i < reps[y].length; i++) {
         
           
-         
-
-          fill(lerpColor(c1, c2, (i+1) / 20));
+          fill(lerpColor(c1, c2, (i) / 10));
           text(phrase, reps[y][i], y * 30 + 30);
-          reps[y][i] += y % 2 ? 1 : -1;
+          reps[y][i] += y % 2 ? .5 : -.5;
 
           if(reps[y][i] > 800) reps[y][i] = -pwidth;
           else if(reps[y][i] < -pwidth) reps[y][i] = 800;
