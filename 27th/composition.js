d1e89824ea36ad95086fae1e2145fa4f849de53d
Travis Faas <travis.faas@gmail.com>
2022-01-28 07:57:18 -0500

    Finishng 27th

--- 27th/composition.js
+++ 27th/composition.js
@@ -4,29 +4,53 @@ let vx = 1;
 let vy = 1;
 let boxes = [];
 let colors = ["#2E294E", "#541388", "#F1E9DA", "#FFD400", "#D90368"];
+let c1s, c2s;
+let ps;
 
 function setup() {
-  createCanvas(1200, 722);
+  createCanvas(800, 200);
   angleMode(DEGREES);
 
 
   background(45);
+  textSize(30);
+
+  c1s = colors;
+  c2s = [];
+  ps = [];
+  for(var i = 0; i < colors.length; i++) {
+    c2s.push(random(colors));
+    ps.push(0);
+  }
 
 
+  translate(20,20);
 }
 
 function draw() {
   
-  //background(255,255,255,.7);
+  //background(255,255,255);
 
-  translate(20,20);
-
+  
   for(let x = 0; x < 10; x++) {
     for(let i = 0; i < colors.length; i++) {
-      fill(colors[i]);
-      text(colors[i], 80 * x, 40 * i); 
+      if(x % 2 == 0) {
+        fill(colors[i]);
+      } else {
+        fill(c2s[i]);
+      }
+      
+      text(colors[i], 120 * x, (40 * i) + ps[x]); 
     }
+    ps[x] += x/3;
+    if(ps[x] > 80) ps[x] = 0;
+    colors.unshift(colors.pop());
   }
 
+  if(frameCount % 10 == 0) {
+    colors.unshift(colors.pop());
+    //colors.unshift(c2s.pop());
+    //c2s.unshift(colors.pop());
+  }
   
 }
